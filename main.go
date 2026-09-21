c1860e9f2fc9c708f1178f09dd242608e09a77b1
Ghiffary Ahmad Muzakky <ghiffaryahmadmuzakky@gmail.com>
2026-09-21 14:08:10 +0700

    API Versioning

--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 	router := gin.Default()
 
-	//anonymous function and routing
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"messsage": "Healthy",
-		})
-	})
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			//anonymous function and routing
+			v1.GET("/", func(ctx *gin.Context) {
+				ctx.JSON(http.StatusOK, gin.H{
+					"messsage": "Healthy",
+				})
+			})
 
-	//named function and routing
-	router.GET("/message", getMessage)
+			//named function and routing
+			v1.GET("/message", getMessage)
 
-	//path variables
-	router.GET("/getId/:id", getId)
-	router.POST("/books", postBookHandler)
-
+			//path variables
+			v1.GET("/getId/:id", getId)
+			v1.POST("/books", postBookHandler)
+		}
+	}
 	router.Run()
 }
 
@@ -49,6 +56,10 @@ type BookInput struct {
 	Title string `json:"title" binding:"required,"`
 	Price int    `json:"price" binding:"required,number"`
 
+	// bisa menggunakan json.[Item] untuk menghindari internal server error
+	// akibat error yang dilewati oleh go validator
+	PageCount json.Number `json:"pageCount" binding:"required,number"`
+
 	//alias JSON dalam go
 	//Subtitle string `json:"sub_title"`
 }
@@ -60,9 +71,16 @@ func postBookHandler(ctx *gin.Context) {
 	if err != nil {
 		//not best practice, karena server akan mati / crash
 		// log.Fatal(err)
-		ctx.JSON(http.StatusBadRequest, err)
-		fmt.Print(err)
-		return //biar ngga eksekusi code dibawahnya
+		// ctx.JSON(http.StatusBadRequest, err)
+		// fmt.Print(err)
+		// return //biar ngga eksekusi code dibawahnya
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errMessage := fmt.Sprintf(e.Field(), e.ActualTag())
+			ctx.JSON(http.StatusBadRequest, errMessage)
+			return
+		}
+
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
